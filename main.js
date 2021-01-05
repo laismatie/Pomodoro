2f871035e39f9d7c9c6b8971fdaa6df24b9d59b6
Laís Matie Hara <lmatiehara@gmail.com>
2021-01-05 10:57:33 -0400

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,8 @@
+var minutos = 25;
+var segundos = "00";
+
+function timer(){
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+}
